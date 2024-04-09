1dcb0849a2ea2c2f1634c6797ca28d222b67a1c5
Silas Reinagel <silas.reinagel@gmail.com>
2024-04-09 13:59:29 -0500

    Tried TTS Demo with HD Voice

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 // @ts-check
-import { createOpenAi } from "./openaiClient";
+import { createOpenAi } from "./openAiClient.js";
 
-const openAi = createOpenAi()
+import fs from "fs";
+import path from "path";
 
-console.log('Hello World!');
+const openai = createOpenAi()
+
+const speechFile = path.resolve("./_output/speech.mp3");
+
+async function main() {
+  const mp3 = await openai.audio.speech.create({
+    model: "tts-1-hd",
+    voice: "onyx",
+    input: "Today is a wonderful day to build something people love!",
+  });
+  console.log(speechFile);
+  const buffer = Buffer.from(await mp3.arrayBuffer());
+  await fs.promises.writeFile(speechFile, buffer);
+}
+
+main();
